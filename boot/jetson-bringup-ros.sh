e9be09f05591f69c58c7ab2512c2b8b6c0daa7df
Nicolas Norambuena <nicolas.norambuenaa@gmail.com>
2025-02-04 18:32:06 -0300

    feat: :sparkles: Renombrar scripts y actualizar la configuración de ROS Master para la gestión de Jetson.

--- boot/jetson-bringup-ros.sh
+++ boot/jetson-bringup-ros.sh
@@ -11,7 +11,7 @@ sudo chmod 660 /dev/gpiochip1
 sudo usermod -a -G dialout puma
 sudo chmod a+rw /dev/ttyUSB0
 
-ROS_MASTER="10.42.0.100"
+ROS_MASTER="10.42.0.199"
 PORT="11311"
 
 echo "Esperando a que el ROS Master ($ROS_MASTER:$PORT) esté disponible..."
@@ -19,5 +19,5 @@ while ! nc -z $ROS_MASTER $PORT; do
   sleep 2
 done
 
-roslaunch puma_bringup_jetson jetson_bringup.launch
+roslaunch puma_bringup_jetson puma_bringup.launch
 
