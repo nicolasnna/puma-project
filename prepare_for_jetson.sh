a1bcc8eea547f21fe065b33811c2f3b39ab0b9da
Nicolas Norambuena <nicolas.norambuenaa@gmail.com>
2025-02-04 17:20:29 -0300

    feat: :sparkles: Mejorar el script de preparación para Jetson, eliminando paquetes extras y ajustando la gestión de permisos.

--- prepare_for_jetson.sh
+++ prepare_for_jetson.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
 echo "Borrando paquetes extras"
 rm -rf puma_arduino
 rm -rf puma_simulation
 rm -rf puma_minipc
-# echo "Cambiando permisos a los scripts"
+
+echo "Terminado"
+echo "Cambiando permisos a los scripts"
 
 # Definir ruta absoluta del script actual
-# SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Llamar al script make_exec.sh usando ruta absoluta
-# source "$SCRIPT_DIR/puma_jetson/make_exec.sh" || echo "No se encuentra 'make_exec.sh'"
+source "$SCRIPT_DIR/puma_jetson/make_exec.sh" || echo "No se encuentra 'make_exec.sh'"
 
 echo "Terminado"
