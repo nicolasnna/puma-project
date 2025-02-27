04c6357ddda7bda2bdb1279981f208d7be311101
Nicolas Norambuena <nicolas.norambuenaa@gmail.com>
2025-02-27 08:39:47 -0300

    feat: :sparkles: Añadir gestor de localización y ajustar parámetros de planificación en el robot PUMA. Arreglado ajuste de orientacion para uso con y sin gps.

--- puma_minipc/puma_nav_manager/scripts/manager_localization_node.py
+++ puma_minipc/puma_nav_manager/scripts/manager_localization_node.py
@@ -4,6 +4,8 @@ from robot_localization.srv import SetPose
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from std_msgs.msg import Float64
 from nav_msgs.msg import Odometry
+import actionlib
+from puma_nav_manager.msg import LocalizationManagerAction, LocalizationManagerFeedback, LocalizationManagerResult, LocalizationManagerGoal
 import math
 
 def set_pose_callback(msg):
@@ -17,9 +19,22 @@ def set_pose_callback(msg):
 def odom_cb(msg):
   global odom
   odom = msg
-    
+  
+def odom_map_cb(msg):
+  global odom_map
+  odom_map = msg
+
+def use_service_set_pose(newPose, ns_service):
+  try:
+    rospy.wait_for_service(f'{ns_service}/set_pose', timeout=5)
+    set_pose = rospy.ServiceProxy(f'{ns_service}/set_pose', SetPose)
+    set_pose(newPose)
+    set_pose.close()
+  except rospy.ServiceException as e:
+    rospy.logerr(f"Service call failed: {e}")
+
 def change_angle_degree_cb(msg):
-  global odom
+  global odom, odom_map
   newPose = PoseWithCovarianceStamped()
   newPose.pose.pose.position.x = odom.pose.pose.position.x
   newPose.pose.pose.position.y = odom.pose.pose.position.y
@@ -29,19 +44,75 @@ def change_angle_degree_cb(msg):
   newPose.pose.pose.orientation.z = math.sin(math.radians(-msg.data)/2)
   newPose.pose.pose.orientation.w = math.cos(math.radians(-msg.data)/2)
   newPose.header.stamp = rospy.Time.now()
-  newPose.header.frame_id = 'odom'
-  try:
-    rospy.wait_for_service('/set_pose', timeout=5)
-    set_pose = rospy.ServiceProxy('/set_pose', SetPose)
-    set_pose(newPose)
-  except rospy.ServiceException as e:
-    rospy.logerr(f"Service call failed: {e}")
+  newPose.header.frame_id = odom.header.frame_id
   
+  use_service_set_pose(newPose, 'puma_ekf_odom')
+  
+  if odom_map:
+    newPose.header.frame_id = odom_map.header.frame_id
+    newPose.pose.pose.position.x = odom_map.pose.pose.position.x
+    newPose.pose.pose.position.y = odom_map.pose.pose.position.y
+    newPose.pose.pose.position.z = odom_map.pose.pose.position.z
+    use_service_set_pose(newPose, 'puma_ekf_map')
+  
+def reset_position_cb():
+  global odom, odom_map
+  newPoseOdom = odom
+  newPoseOdom.pose.pose.position.x = 0
+  newPoseOdom.pose.pose.position.y = 0
+  newPoseOdom.pose.pose.position.z = 0
+  use_service_set_pose(newPoseOdom, 'puma_ekf_odom')
+  if odom_map:
+    newPoseMap = odom_map
+    newPoseMap.pose.pose.position.x = 0
+    newPoseMap.pose.pose.position.y = 0
+    newPoseMap.pose.pose.position.z = 0
+    use_service_set_pose(newPoseMap, 'puma_ekf_map')
+  
+def server_cb(goal: LocalizationManagerGoal):
+  global srv
+  result = LocalizationManagerResult()
+  
+  try: 
+    if goal.action == LocalizationManagerGoal.ACTION_CHANGE_ANGLE:
+      change_angle_degree_cb(goal.angle_deg)
+      result.success = True
+      result.message = 'Ángulo cambiado con éxito'
+    elif goal.action == LocalizationManagerGoal.ACTION_RESET_POSITION:
+      reset_position_cb()
+      result.success = True
+      result.message = 'Posición reiniciada con éxito'
+    else:
+      result.success = False
+      result.message = 'Acción no válida'
     
-if __name__ == '__main__':
+    srv.set_succeeded(result)
+  
+  except Exception as e:
+    rospy.logwarn(f"Error al ejecutar acción: {e}")
+    result.success = False
+    result.message = 'Error al ejecutar acción'
+    srv.set_succeeded(result)
+  
+  
+def main():
   rospy.init_node('manage_localization_ekf')
+  
+  global odom, odom_map, srv
+  odom = odom_map = None
+  
   rospy.Subscriber('/puma/localization/set_new_pose', PoseWithCovarianceStamped, set_pose_callback)
   rospy.Subscriber('/puma/localization/change_angle_degree', Float64, change_angle_degree_cb)
   rospy.Subscriber('/puma/localization/ekf_odometry', Odometry, odom_cb)
+  rospy.Subscriber('/puma/localization/filtered_map', Odometry, odom_map_cb)
+  
+  rospy.loginfo("Nodo manager_localization iniciado")
+  
+  
+  srv = actionlib.SimpleActionServer('/puma/localization/manager', LocalizationManagerAction, server_cb, False)
+  srv.start()
   
   rospy.spin()
+
+if __name__ == '__main__':
+  main()
--- puma_minipc/puma_web_interface/scripts/puma_web_interface/translate_command.py
+++ puma_minipc/puma_web_interface/scripts/puma_web_interface/translate_command.py
@@ -3,6 +3,7 @@ from std_msgs.msg import Empty, String, Float64
 from puma_msgs.msg import ConfigurationStateMachine, WaypointNav, Waypoint
 from puma_state_machine.msg import StateMachineAction, StateMachineGoal
 from smach_msgs.msg import SmachContainerStatus
+from puma_nav_manager.msg import LocalizationManagerAction, LocalizationManagerGoal
 import actionlib
 
 # ------ Publishers ------
@@ -18,7 +19,8 @@ change_mode_pub = rospy.Publisher("/puma/control/change_mode", String, queue_siz
 # ------ Client action ------
 client_sm_plan_config = actionlib.SimpleActionClient('/puma/state_machine/plan_configuration', StateMachineAction)
 client_sm_run_plan = actionlib.SimpleActionClient('/puma/state_machine/run_plan', StateMachineAction)
-clien_sm_run_plan_custom = actionlib.SimpleActionClient('/puma/state_machine/run_plan_custom', StateMachineAction)
+client_sm_run_plan_custom = actionlib.SimpleActionClient('/puma/state_machine/run_plan_custom', StateMachineAction)
+client_localization_manager = actionlib.SimpleActionClient('/puma/localization/manager', LocalizationManagerAction)
 
 def action_state_machine(client,goal):
   try:
@@ -55,7 +57,7 @@ def stop_plan_fn(cmd):
   if state == "RUN_PLAN":
     return action_state_machine(client_sm_run_plan, goal)
   elif state == "RUN_PLAN_CUSTOM":
-    return action_state_machine(clien_sm_run_plan_custom, goal)
+    return action_state_machine(client_sm_run_plan_custom, goal)
   return False
   
 def clear_plan_fn(cmd):
@@ -118,8 +120,18 @@ def add_waypoints_fn(waypoints):
   
 def change_angle_degree_fn(cmd):
   rospy.loginfo(f"Detectado comando para cambiar orientación {cmd}")
-  set_orientation_localization_pub.publish(Float64(cmd["angle"]))
-  return True
+  try:
+    client_localization_manager.wait_for_server(rospy.Duration(5))
+    goal = LocalizationManagerGoal()
+    goal.action = LocalizationManagerGoal.ACTION_CHANGE_ANGLE
+    goal.angle_deg = Float64(cmd["angle"])
+    client_localization_manager.send_goal(goal)
+    if client_localization_manager.wait_for_result(rospy.Duration(5)):
+      return True
+    return False
+  except Exception as e:
+    rospy.logwarn(f"Error al conectar con el servidor: {e}")
+    return False
   
 def change_mode_fn(cmd):
   rospy.loginfo(f"Detectado comando para cambiar modo {cmd}")
