c93faae9874ea6f7b1fa5f7a69ecc9772d630330
Nicolas Norambuena <nicolas.norambuenaa@gmail.com>
2025-05-05 17:00:11 -0400

    feat: :sparkles: Añadido el servicio 'puma-arduino-nano' y su script correspondiente; actualizado el archivo de lanzamiento para mejorar la integración del sistema.

--- install/minipc/install_services_minipc.sh
+++ install/minipc/install_services_minipc.sh
@@ -28,6 +28,7 @@ declare -A servicios=(
   ["10"]="puma-visual-odometry|${FILES_DIR}/puma-visual-odometry.service|${SERVICE_DST}/puma-visual-odometry.service|${FILES_DIR}/puma-visual-odometry.sh|${SCRIPT_DST}/puma-visual-odometry.sh"
   ["11"]="puma-web-interface|${FILES_DIR}/puma-web-interface.service|${SERVICE_DST}/puma-web-interface.service|${FILES_DIR}/puma-web-interface.sh|${SCRIPT_DST}/puma-web-interface.sh"
   ["12"]="puma-minipc-monitor|${FILES_DIR}/puma-minipc-monitor.service|${SERVICE_DST}/puma-minipc-monitor.service|${FILES_DIR}/puma-minipc-monitor.sh|${SCRIPT_DST}/puma-minipc-monitor.sh"
+  ["13"]="puma-arduino-nano|${FILES_DIR}/puma-arduino-nano.service|${SERVICE_DST}/puma-arduino-nano.service|${FILES_DIR}/puma-arduino-nano.sh|${SCRIPT_DST}/puma-arduino-nano.sh"
 )
 
 mostrar_menu() {
