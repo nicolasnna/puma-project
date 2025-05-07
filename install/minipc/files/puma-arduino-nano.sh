dfac5d8801926c8730a5e455b1df0cd40d5bf630
Nicolas Norambuena <nicolas.norambuenaa@gmail.com>
2025-05-07 09:30:54 -0400

    feat: :sparkles: Añadido el bloque de permisos para '/dev/ttyUSB0' en 'puma-arduino-nano.sh' para asegurar el acceso adecuado al dispositivo antes de iniciar el arduino nano.

--- install/minipc/files/puma-arduino-nano.sh
+++ install/minipc/files/puma-arduino-nano.sh
@@ -7,6 +7,10 @@ source /home/puma/puma_ws/devel/setup.bash
 ROS_MASTER="10.42.0.100"
 PORT="11311"
 
+if [ -e /dev/ttyUSB0 ]; then
+  sudo chmod a+rw /dev/ttyUSB0
+fi
+
 echo "Esperando a que el ROS Master ($ROS_MASTER:$PORT) esté disponible..."
 while ! nc -z $ROS_MASTER $PORT; do
   sleep 2
