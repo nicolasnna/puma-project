f8659d7ecba6904c60473dce3078500561b73f32
Nicolas Norambuena <nicolas.norambuenaa@gmail.com>
2025-05-10 15:28:55 -0400

    feat: :sparkles: Añadido el manejador de dispositivos IP y el script correspondiente, junto con la integración en el sistema de servicios.

--- puma_jetson/puma_ip_devices/scripts/speaker_manager_node.py
+++ puma_jetson/puma_ip_devices/scripts/speaker_manager_node.py
@@ -6,31 +6,76 @@ from puma_msgs.msg import Log
 from puma_ip_devices.msg import SpeakerManagerAction, SpeakerManagerGoal, SpeakerManagerResult
 from puma_ip_devices.utils import send_log_message
 
+def send_to_speaker(url):
+  try:
+    rs = requests.get(url, timeout=5)
+    if rs.status_code == 200:
+      send_log_message(f"Se ha realizado la petición al parlante correctamente.")
+      return True
+  except requests.exceptions.RequestException as e:
+    send_log_message(f"Error al obtener datos al realizar peticion al parlante: {e}")
+  return False
+
 def play_speaker(goal: SpeakerManagerGoal):
   ip = rospy.get_param('~ip_speaker', '10.42.0.95')
+  url = f"http://{ip}/api/play?action=start"
   result = SpeakerManagerResult()
   if not goal.filename:
     result.message = 'Error: Es necesario indicar el nombre del archivo'
     result.success = False
+    return result
   
+  url = url + f"&file={goal.filename}"
   
+  if goal.mode == SpeakerManagerGoal.MODE_PLAY_REPEAT:
+    url = url + f"&mode={goal.mode}"
+    if not goal.repeats or goal.repeats == 0:
+      result.message = f'Error: Para el modo {goal.mode} requiere indicar repeticiones mayores a 0'
+      result.success = False
+      return result
+    url = url + f"&count={goal.repeats}"
+    
+  elif goal.mode == SpeakerManagerGoal.MODE_PLAY_TIME:
+    url = url + f"&mode={goal.mode}"
+    if not goal.time_seconds or goal.time_seconds == 0:
+      result.message = f'Error: Para el modo {goal.mode} requiere indicar el tiempo de sonido a 0'
+      result.success = False
+      return result
+    url = url + f"&count={goal.time_seconds}"
+  
+  if goal.volume and goal.volume >= 1 and goal.volume <= 100:
+    url = url + f"&volume={goal.volume}"
+  
+  is_work = send_to_speaker(url)
+  
+  result.success = is_work
+  result.message = "Se ha realizado la petición al parlante correctamente" if is_work else "No se ha logrado realizar la petición al parlante"
+  return result
 
 def stop_speaker():
   ip = rospy.get_param('~ip_speaker', '10.42.0.95')
-  pass
+  url = f"http://{ip}/api/play?action=stop"
+  result = SpeakerManagerResult()
+  is_work = send_to_speaker(url)
+  
+  result.success = is_work
+  result.message = "Se ha realizado la petición de parada al parlante" if is_work else "No se ha logrado realizar la petición al parlante"
+  return result
 
 def speaker_srv_cb(goal: SpeakerManagerGoal):
   result = SpeakerManagerResult()
   
   if goal.action == SpeakerManagerGoal.ACTION_PLAY:
-    play_speaker()
+    result = play_speaker()
   elif goal.action == SpeakerManagerGoal.ACTION_STOP:
-    stop_speaker()
+    result = stop_speaker()
   else:
     result.success = False
     result.message = "Comando 'action' no soportado"
     srv.set_aborted(result)
+    return False
   
+  srv.set_succeeded(result)
 
 def main():
   rospy.init_node('speaker_manager_node')
